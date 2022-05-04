809b51fdf43471eeddc3f0294f7421e8d579ab37
cmwschroeder <cmwschroeder@gmail.com>
2022-05-04 16:54:49 -0700

    made it so that post requests for tags would create a new tag in the db and we could link products to the new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,8 +34,33 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  /* req body should look like: 
+    {
+      tag_name: "Tag",
+    }
+  */
+  try {
+  const tag = await Tag.create(req.body);
+  //if there are product ids to add this tag to then do that
+  if(req.body.productIds.length) {
+    const productTagIdArr = req.body.productIds.map((product_id) => {
+      return {
+        tag_id: tag.id,
+        product_id,
+      };
+    });
+    res.json(await ProductTag.bulkCreate(productTagIdArr));
+  }
+  //or else just return the created tag
+  else {
+    res.json(tag);
+  }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+    
 });
 
 router.put('/:id', (req, res) => {
