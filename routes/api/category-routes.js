91232f4f4fa1bad7f8423dccf0f640bdc53de07d
cmwschroeder <cmwschroeder@gmail.com>
2022-05-03 22:41:52 -0700

    created product model, made it so that get requests for categories include products, added get request for specific category id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,14 +7,31 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: [{ model: Product}],
+    });
     res.json(categoryData);
-  } catch (err) {}
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    });
+
+    if(!categoryData) {
+      res.status(404).json({ message: 'No category with that id'});
+      return;
+    }
+    res.json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
