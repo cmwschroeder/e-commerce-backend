1cb89f03b409a0f8ca6e673434d4c9e70619221e
cmwschroeder <cmwschroeder@gmail.com>
2022-05-04 23:14:10 -0700

    added create, update, and deleting categories, made is so that on category delete, the product in the category is also deleted

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,16 +34,48 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  /* req.body should look like this...
+    {
+      category_name: "Sports",
+    }
+  */
+  try {
+    const category = await Category.create(req.body);
+    res.json(category);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json("Category Updated");
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    await Category.destroy({
+      where: { id: req.params.id }
+    });
+    res.json("Category Deleted");
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
