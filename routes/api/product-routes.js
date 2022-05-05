74ceae653962fbb7047e4043dcc52e2940f684a0
cmwschroeder <cmwschroeder@gmail.com>
2022-05-04 22:48:32 -0700

    added delete routes for products and tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -110,8 +110,17 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    await Product.destroy({
+      where: { id: req.params.id }
+    });
+    res.json("Product deleted");
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -97,8 +97,17 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    await Tag.destroy({
+      where: { id: req.params.id }
+    });
+    res.json("Tag deleted");
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
